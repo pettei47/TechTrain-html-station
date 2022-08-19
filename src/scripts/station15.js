8a1eef33d1d6c25902c350fb3c64a26fb7fca657
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-08-20 00:46:00 +0900

    15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  await wait(3)
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return userList.map(user => {
+    user.full_name = `${user.family_name} ${user.first_name}`
+    return user
+  })
 }
 
+function wait(sec) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, sec * 1000)
+  })
+}
